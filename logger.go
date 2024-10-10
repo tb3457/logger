e618bd86549290dcea70f178fd5eeebaa276c036
tb3457 <97063406+tb3457@users.noreply.github.com>
2024-10-10 12:56:14 +0300

    Add levels

--- logger.go
+++ logger.go
@@ -10,6 +10,27 @@ var log *logrus.Logger
 
 type Level logrus.Level
 
+const (
+	// PanicLevel level, highest level of severity. Logs and then calls panic with the
+	// message passed to Debug, Info, ...
+	PanicLevel Level = iota
+	// FatalLevel level. Logs and then calls `logger.Exit(1)`. It will exit even if the
+	// logging level is set to Panic.
+	FatalLevel
+	// ErrorLevel level. Logs. Used for errors that should definitely be noted.
+	// Commonly used for hooks to send errors to an error tracking service.
+	ErrorLevel
+	// WarnLevel level. Non-critical entries that deserve eyes.
+	WarnLevel
+	// InfoLevel level. General operational entries about what's going on inside the
+	// application.
+	InfoLevel
+	// DebugLevel level. Usually only enabled when debugging. Very verbose logging.
+	DebugLevel
+	// TraceLevel level. Designates finer-grained informational events than the Debug.
+	TraceLevel
+)
+
 func Register(level Level, file *os.File) {
 	Out := io.MultiWriter(os.Stdout)
 	if file != nil {
